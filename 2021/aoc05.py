fee8df802ec89325af9ba752c15f4e8e6f26d80e
Mattias Fransson <mattias@webben.nu>
2021-12-06 23:03:04 +0100

    Got a number, but it's still to low

--- 2021/aoc05.py
+++ 2021/aoc05.py
@@ -1,7 +1,9 @@
 #!/usr/local/bin/python3
+from functools import reduce
+
 def readfile():
   coordinates = []
-  #720,475 -> 720,669
+  #Example line: 720,475 -> 720,669
   with open('aoc05.txt', 'r') as file:
     for line in file:
       start, stop = line.strip().split(' -> ')
@@ -13,6 +15,23 @@ def readfile():
 def first():
   coordinates = readfile()
   # remove diagonal lines
-  coordinates = filter(lambda line: line[0][0] == line[1][0] or line[0][1] == line[1][1], coordinates)
+  coordinates = list(filter(lambda line: line[0][0] == line[1][0] or line[0][1] == line[1][1], coordinates))
+  maxcoordinate = max(reduce(lambda a, b: a+b, reduce(lambda a, b: a+b, coordinates)))+2
+  diagram = []
+  for i in range(maxcoordinate):
+    diagram.append(maxcoordinate * [0])
+  for line in coordinates:
+    start, stop = line
+    x1, y1 = start
+    x2, y2 = stop
+    if x1 == x2:
+      x = x1
+      for y in range(y1, y2+1):
+        diagram[y][x] += 1
+    elif y1 == y2:
+      y = y1
+      for x in range(x1, x2+1):
+        diagram[y][x] += 1
+  print(f"{len(list(filter(lambda z: z > 1, reduce(lambda a, b: a+b, diagram))))}")
 
 first()
