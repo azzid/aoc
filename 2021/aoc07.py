ccdb2537f5db75d56bf60e6381fe682e8cef2867
Mattias Fransson <mattias@webben.nu>
2021-12-08 21:03:41 +0100

    day 7 part 1 done

--- 2021/aoc07.py
+++ 2021/aoc07.py
@@ -5,7 +5,16 @@ def readfile():
     crabs = list(map(int, file.read().strip().split(',')))
   return crabs
 
+def fuelcost(alignto, crabs):
+  fuelconsumed = 0
+  for crab in crabs:
+    fuelconsumed += max(alignto, crab) - min(alignto, crab)
+  return fuelconsumed
+
 def first():
   crabs = readfile()
+  fuelcosts = [(pos, fuelcost(pos, crabs)) for pos in range(2000)]
+  cheapest = min(fuelcosts,key=lambda fuelcost:fuelcost[1])
+  print(f"Cheapest position is {cheapest[0]} which requires {cheapest[1]} units of fuel")
 
 first()
