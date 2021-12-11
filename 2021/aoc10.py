392150d56eff63f1f6f6b5eae56ad411968093fb
Mattias Fransson <mattias@webben.nu>
2021-12-11 10:24:22 +0100

    That's not the right answer; your answer is too high. (You guessed 282288899348.)

--- 2021/aoc10.py
+++ 2021/aoc10.py
@@ -48,5 +48,55 @@ def first():
 
 def second():
   data = readfile()
+  openings = '([{<'
+  closings = ')]}>'
+  points = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
+  }
+  seen = []
+  totalscore = 0
+  for line in data:
+    for char in line:
+      if char in openings:
+        seen.append(char)
+      elif char in closings:
+        checkifmatch = seen.pop()
+        if checkifmatch == '(' and char == ')':
+          pass
+        elif checkifmatch == '[' and char == ']':
+          pass
+        elif checkifmatch == '{' and char == '}':
+          pass
+        elif checkifmatch == '<' and char == '>':
+          pass
+        else:
+          seen = []
+          break
+          #print(f"{checkifmatch} does not match {char}, scoring {points[char]} points.")
+          #print(f"Line: {''.join(line)} is bust.")
+          #quit()
+    missingclosings = []
+    while seen:
+    # Still got unclosed openings, line need to be completed
+      opening = seen.pop()
+      if opening == '(':
+        missingclosings.append(')')
+      elif opening == '[':
+        missingclosings.append(']')
+      elif opening == '{':
+        missingclosings.append('}')
+      elif opening == '<':
+        missingclosings.append('>')
+    if missingclosings:
+      #print(f"{''.join(missingclosings)}")
+      score = 0
+      for c in missingclosings:
+        score *= 5
+        score += points[c]
+      totalscore += score
+  print(f"Score: {totalscore}")
 
-first()
+second()
