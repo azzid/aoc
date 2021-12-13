ca8520a891d234a1e379fd48a654ec845994736d
Mattias Fransson <mattias@webben.nu>
2021-12-13 20:10:53 +0100

    possible next edges listed

--- 2021/aoc12.py
+++ 2021/aoc12.py
@@ -13,9 +13,26 @@ def readfile():
       data.append(tuple(line.strip().split('-')))
   return data
 
+def possiblenext(path, data):
+  # Impossibly clever way to keep track of visited small caves
+  visitedsmallcaves = list(set([point for edge in path for point in edge if point.islower()]))
+  if not path:
+    # Path not started, first edge[0] is 'start' mandatory
+    starts = [edge for edge in data if edge[0] == 'start']
+    return starts
+  if path[-1][1] == 'stop':
+    # Path has reached stop. No possible next step.
+    return []
+  else:
+    nexts = [edge for edge in data if path[-1][1] == edge[0] and not edge[1] in visitedsmallcaves]
+    return nexts
+
 def first():
   data = readfile()
-  print(f"{data[0]}")
+  path = []
+  path.append(possiblenext(path, data)[0])
+  print(f"{possiblenext(path, data)}")
+  
 
 def second():
   data = readfile()
