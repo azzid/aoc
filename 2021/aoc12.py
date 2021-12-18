203dc2317a8e6b8313e27390481257de58d876ca
Mattias Fransson <mattias@webben.nu>
2021-12-18 11:47:49 +0100

    set up for part 2

--- 2021/aoc12.py
+++ 2021/aoc12.py
@@ -34,6 +34,24 @@ def possiblenext(path):
     else:
       return ["DEAD END"]
 
+def possiblenext2(path):
+  global data
+  # Impossibly clever way to keep track of visited small caves
+  visitedsmallcaves = list(set([point for edge in path for point in edge if point.islower()]))
+  if not path:
+    # Path not started, first edge[0] is 'start' mandatory
+    starts = [edge for edge in data if edge[0] == 'start']
+    return starts
+  elif path[-1][-1] == 'end' or 'DEAD END' in path:
+    # Path has reached end. No next step.
+    return []
+  else:
+    nexts = [edge for edge in data if path[-1][1] == edge[0] and not edge[1] in visitedsmallcaves]
+    if nexts:
+      return nexts
+    else:
+      return ["DEAD END"]
+
 def first():
   readfile()
   path = []
@@ -55,6 +73,23 @@ def first():
   print(f"{len(paths)}")
   
 def second():
-  data = readfile()
+  readfile()
+  path = []
+  paths = []
+  # Seed paths with start edges
+  for nextstep in possiblenext2(path):
+    path.append(nextstep)
+    paths.append(deepcopy(path))
+    path.remove(nextstep)
+  unfinishedpaths = deepcopy(paths)
+  while unfinishedpaths:
+    for path in unfinishedpaths:
+      paths.remove(path)
+      for nextstep in possiblenext2(path):
+        path.append(nextstep)
+        paths.append(deepcopy(path))
+        path.remove(nextstep)
+    unfinishedpaths = [path for path in paths if not path[-1][-1] == 'end']
+  print(f"{len(paths)}")
 
-first()
+second()
