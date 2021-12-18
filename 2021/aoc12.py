a81294d04c34c5f518852ce970cf97816baa18dc
Mattias Fransson <mattias@webben.nu>
2021-12-18 11:30:28 +0100

    Turns out tunnels goes two ways - part 1 done

--- 2021/aoc12.py
+++ 2021/aoc12.py
@@ -12,29 +12,31 @@ def readfile():
   datafile = datafilename()
   with open(datafile, 'r') as file:
     for line in file:
-      data.append(tuple(line.strip().split('-')))
+      points = tuple(line.strip().split('-'))
+      data.append(tuple((points[0], points[1])))
+      data.append(tuple((points[1], points[0])))
 
 def possiblenext(path):
   global data
   # Impossibly clever way to keep track of visited small caves
   visitedsmallcaves = list(set([point for edge in path for point in edge if point.islower()]))
-  #print(f"path: {path}")
+  #print(f"path: {path} has visited {visitedsmallcaves} small caves")
   if not path:
     # Path not started, first edge[0] is 'start' mandatory
     starts = [edge for edge in data if edge[0] == 'start']
-    print(f"path {path} has not started, return starts ({starts})")
+    #print(f"path {path} has not started, return starts ({starts})")
     return starts
-  elif path[-1][1] == 'end' or 'DEAD END' in path:
+  elif path[-1][-1] == 'end' or 'DEAD END' in path:
     # Path has reached end. No next step.
-    print(f"Path has reached end. No next step in {path}")
+    #print(f"Path has reached end. No next step in {path}")
     return []
   else:
     nexts = [edge for edge in data if path[-1][1] == edge[0] and not edge[1] in visitedsmallcaves]
     if nexts:
-      print(f"path {path} can continue via {nexts}")
+      #print(f"path {path} can continue via {nexts}")
       return nexts
     else:
-      print(f"path {path} seem to be a dead end")
+      #print(f"path {path} seem to be a dead end")
       return ["DEAD END"]
 
 def first():
@@ -46,17 +48,20 @@ def first():
     path.append(nextstep)
     paths.append(deepcopy(path))
     path.remove(nextstep)
-  while [path for path in paths if not path[-1][-1] == 'end']:
-    for path in [path for path in paths if not path[-1][-1] == 'end']:
-      print(f"current unfinished path: {path}")
+  unfinishedpaths = [path for path in paths if not path[-1][-1] == 'end']
+  #print(f"unfinished: {unfinishedpaths}")
+  while unfinishedpaths:
+    for path in unfinishedpaths:
+      #print(f"current unfinished path: {path}")
       paths.remove(path)
       for nextstep in possiblenext(path):
         path.append(nextstep)
         paths.append(deepcopy(path))
         path.remove(nextstep)
+    unfinishedpaths = [path for path in paths if not path[-1][-1] == 'end']
+    #print(f"unfinished: {unfinishedpaths}")
   print(f"{len(paths)}")
   
-
 def second():
   data = readfile()
 
