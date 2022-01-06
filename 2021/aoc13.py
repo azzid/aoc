d2229c5ff94b906fd534d0e2b4cc5c0620184425
Mattias Fransson <mattias@webben.nu>
2022-01-06 14:22:11 +0100

    Put points on paper
    and learn once again that lists in Python are weird

--- 2021/aoc13.py
+++ 2021/aoc13.py
@@ -21,13 +21,25 @@ def readfile():
         folds.append(fold)
   return points, folds
 
-def makeblankmap(x, y):
-  return [[False] * x] * y
+def makeblankpaper(x, y):
+  line = []
+  lines = []
+  for i in range(x):
+    line.append(False)
+  for i in range(y):
+    lines.append(line[:])
+  return lines
+
+def putpointsonpaper(points, paperarray):
+  for point in points:
+    paperarray[point[1]][point[0]] = True
 
 def first():
   points, folds = readfile()
   maxX = max([point[0] for point in points])
   maxY = max([point[1] for point in points])
-  maparray = makeblankmap(maxX+1, maxY+1)
+  paperarray = makeblankpaper(maxX+1, maxY+1)
+  putpointsonpaper(points, paperarray)
+  print(f"{paperarray}")
 
 first()
