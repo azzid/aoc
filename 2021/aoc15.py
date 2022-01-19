44fedf22b2f4b0692494c8cc468007e269ebf5fe
Mattias Fransson <mattias@webben.nu>
2022-01-19 16:11:51 +0100

    found a path - not the right one
    That's not the right answer; your answer is too high. If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 820.)

--- 2021/aoc15.py
+++ 2021/aoc15.py
@@ -12,12 +12,36 @@ def readfile():
   datafile = datafilename()
   with open(datafile, 'r') as file:
     for line in file:
-      riskmap.append(list(line))
+      riskmap.append([int(a) for a in list(line.strip())])
   return riskmap
 
 def first():
   starttime = time()
   riskmap = readfile()
-  print(f"top left: {riskmap[0][0:3]}, bottom right: {riskmap[-1][-4:-1]}")
+  maxX, maxY = len(riskmap[0])-1, len(riskmap)-1
+  pos = [0,0]
+  goal = [maxX, maxY]
+  pathrisk = 0
+  #import code
+  #code.interact(local=locals())
+  #riskmap[pos[1]][pos[0]]
+  #print(f"top left: {riskmap[0][0:3]}, bottom right: {riskmap[-1][-4:-1]}")
+  #print(f"lenX: {maxX}, lenY: {maxY}")
+  #print(f"goal: {riskmap[maxY][maxX]}")
+  while pos[0] < goal[0] or pos[1] < goal[1]:
+    if pos[0] < goal[0] and pos[1] < goal[1]:
+      if riskmap[pos[1]+1][pos[0]] > riskmap[pos[1]][pos[0]+1]:
+        pos[0] += 1
+      elif riskmap[pos[1]+1][pos[0]] <= riskmap[pos[1]][pos[0]+1]:
+        pos[1] += 1
+    elif pos[1] == goal[1]:
+      pos[0] += 1
+    elif pos[0] == goal[0]:
+      pos[1] += 1
+    else:
+      print(f"should probably never get here. quit.")
+      quit()
+    pathrisk += riskmap[pos[1]][pos[0]]
+  print(f"risk: {pathrisk}")
 
 first()
