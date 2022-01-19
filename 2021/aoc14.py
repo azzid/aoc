cd99a0744049728742b03582f2f9a8ca6b5fcee9
Mattias Fransson <mattias@webben.nu>
2022-01-19 09:48:38 +0100

    Add paircounter

--- 2021/aoc14.py
+++ 2021/aoc14.py
@@ -33,18 +33,37 @@ def makeinsertions(template, insertions):
     newstring += template[i] + getinsertion(pair, insertions)
   newstring += pair[-1]
   return newstring
+
+def countpairsinstring(template):
+  paircounts = {}
+  paircounts['first'] = template[0]
+  paircounts['last'] = template[-1]
   
-def first():
+  for i in range(len(template)-1):
+    pair = template[i] + template[i+1]
+    try:
+      paircounts[pair] += 1
+    except KeyError:
+      paircounts[pair] = 1
+  # Remove first/last:
+  # {k: paircounts[k] for k in paircounts if len(k) == 2}
+  return paircounts
+  
+def second():
+  iterattions = 40
   starttime = time()
   template, insertions = readfile()
-  for i in range(40):
+  print(f"{countpairsinstring(template)}")
+  quit()
+  for i in range(iterations):
     template = makeinsertions(template, insertions)
-    print(f"after step {i+1} lengt of template is {len(template)} and {int(time() - starttime)}s has passed since start")
-  counts = Counter(list(template))
-  maxchar=max(counts, key=counts.get)
-  maxoccur=counts[maxchar]
-  minchar=min(counts, key=counts.get)
-  minoccur=counts[minchar]
-  print(f"difference between number of {maxchar}[{maxoccur}] and {minchar}[{minoccur}] is: {maxoccur-minoccur}")
+    #print(f"after step {i+1} lengt of template is {len(template)} and {int(time() - starttime)}s has passed since start")
+    counts = Counter(list(template))
+    #print(counts)
+    maxchar=max(counts, key=counts.get)
+    maxoccur=counts[maxchar]
+    minchar=min(counts, key=counts.get)
+    minoccur=counts[minchar]
+    print(f"{i+1}: diff {maxchar}[{maxoccur}]-{minchar}[{minoccur}]: {maxoccur-minoccur}")
 
-first()
+second()
