a9218a654b3745e17d6f790620dea260ffdd4b25
Mattias Fransson <mattias@webben.nu>
2022-12-12 16:33:53 +0100

    read data to list

---
+++ 2022/aoc01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from pathlib import Path
+def datafilename():
+  mypath = Path(__file__)
+  txtpath = mypath.with_suffix('.txt')
+  return txtpath
+
+def readfile():
+  data = []
+  backpack = []
+  datafile = datafilename()
+  with open(datafile, 'r') as file:
+    for line in file:
+      if not line.strip():
+        data.append(backpack)
+        backpack = []
+      else:
+        backpack.append(int(line.strip()))
+  return data
+
+print(f"{readfile()}")
