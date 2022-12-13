9ebde66352947944cd7ce606e7d2ac56977b4187
Mattias Fransson <mattias@webben.nu>
2022-12-14 00:01:54 +0100

    3 groundwork

---
+++ 2022/aoc03.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from pathlib import Path
+def datafilename():
+  mypath = Path(__file__)
+  txtpath = mypath.with_suffix('.txt')
+  return txtpath
+
+def readfile():
+  data = []
+  datafile = datafilename()
+  with open(datafile, 'r') as file:
+    for line in file:
+      data.append(split_string(line.strip()))
+  return data
+
+def split_string(s):
+  a = s[0:int(len(s)/2)]
+  b = s[int(len(s)/2):]
+  return tuple((list(a), list(b)))
+
+char_values = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, 'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52}
+
+print(f"{readfile()[0]}")
+print(f"a = {char_values['a']}")
+print(f"z = {char_values['z']}")
+print(f"A = {char_values['A']}")
+print(f"Z = {char_values['Z']}")
