5d210bb245fe8da65999fc2e3eac0d8e3ea309f7
Mattias Fransson <mattias@webben.nu>
2022-12-15 09:11:09 +0100

    That's the right answer!
    You are one gold star closer to collecting enough star fruit.
    Answer: 2689

--- 2022/aoc03.py
+++ 2022/aoc03.py
@@ -9,8 +9,12 @@ def readfile():
   data = []
   datafile = datafilename()
   with open(datafile, 'r') as file:
-    for line in file:
-      data.append(split_string(line.strip()))
+    for lineno, line in enumerate(file):
+      groupid = int(lineno/3)
+      elfid = int(lineno%3)
+      if elfid == 0:
+        data.append([])
+      data[groupid].append(set(list(line.strip())))
   return data
 
 def split_string(s):
@@ -20,13 +24,10 @@ def split_string(s):
 
 char_values = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, 'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39, 'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52}
 
-i = 0
 total = 0
-for rucksack in readfile():
-  i += 1
-  first_compartment_set = set(rucksack[0])
-  second_compartment_set = set(rucksack[1])
-  common_item = first_compartment_set.intersection(second_compartment_set).pop()
-  value = char_values[common_item]
-  total += value
-  print(f"{i}: {common_item} worth {value} gives current total {total}")
+for i, group in enumerate(readfile()):
+  common_item = group[0].intersection(group[1]).intersection(group[2]).pop()
+  item_value = char_values[common_item]
+  total += item_value
+  print(f"group {i} has {common_item} in common, valued at {item_value} - added to the total it lands on {total}")
+
