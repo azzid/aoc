a971f682ec80d9323f783dc90f13c3c0883a0551
Mattias Fransson <mattias@webben.nu>
2022-12-16 21:48:18 +0100

    That's the right answer!
    You are one gold star closer to collecting enough star fruit.
    Answer: 823

--- 2022/aoc04.py
+++ 2022/aoc04.py
@@ -16,6 +16,15 @@ def readfile():
       data.append(tuple((range(astart, astop+1), range(bstart, bstop+1))))
   return data
 
+def does_ranges_overlap(rangetuple):
+  rangea = rangetuple[0]
+  rangeb = rangetuple[1]
+  commonlength = len(set(rangea).intersection(set(rangeb)))
+  if commonlength > 0:
+    return True
+  else:
+    return False
+
 def does_one_contain_the_other(rangetuple):
   rangea = rangetuple[0]
   rangeb = rangetuple[1]
@@ -32,6 +41,6 @@ i = 0
 for rangetuple in readfile():
   if debug:
     print(f"{rangetuple}: {does_one_contain_the_other(rangetuple)}")
-  elif does_one_contain_the_other(rangetuple):
+  elif does_ranges_overlap(rangetuple):
     i += 1
-print(f"found {i} ranges within rangepair")
+print(f"found {i} ranges with overlap")
