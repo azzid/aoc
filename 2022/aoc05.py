830007435346765246e10100df098e7f9057af2f
Mattias Fransson <mattias@webben.nu>
2022-12-17 12:19:57 +0100

    also read move operations from file

--- 2022/aoc05.py
+++ 2022/aoc05.py
@@ -20,23 +20,27 @@ def readfile():
            }
   #indices = list(range(1, len(line), 4))          # hard-code to prevent opening file twice or computing list for every line in file
   indices = [ 1, 5, 9, 13, 17, 21, 25, 29, 33 ]
-  operations = []
+  instruction = []
+  operations = False
   datafile = datafilename()
   with open(datafile, 'r') as file:
     for line in file:
       if not line.strip():
         operations = True
+        continue
       if operations:
         # do operations stuff
-        continue
+        a = line.strip().split(' ')
+        d = { a[0]: a[1], a[2]: a[3], a[4]: a[5] }
+        instruction.append(d)
       else:
         lineobjects = list(itemgetter(*indices)(list(line)))
         for i, x in enumerate(lineobjects):
           if ' ' in x or x in map(str, stacks.keys()):
-            #print(f"{i+1} is {x} - skipping")
             continue
-          #print(f"{i+1}: {x}")
           stacks[i+1].insert(0, x)
-    print(f"{stacks}")
+    return(stacks, instruction)
 
-readfile()
+stacks, operations = readfile()
+print(f"stack 1: {stacks[1]}")
+print(f"operation 1: {operations[1]}")
