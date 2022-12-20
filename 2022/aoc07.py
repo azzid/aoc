9ba628ed5c385eae1a476a9765c94e79b4a79a84
Mattias Fransson <mattias@webben.nu>
2022-12-20 18:30:37 +0100

    might this be something?

--- 2022/aoc07.py
+++ 2022/aoc07.py
@@ -6,18 +6,16 @@ def datafilename():
   return txtpath
 
 def readfile():
-  data = []
   datafile = datafilename()
   with open(datafile, 'r') as file:
-    for line in file:
-      if line[0] == '$':   # command
-        if line.strip().split()[1] == 'cd' and not line.strip().split()[2] == '..':
-          descend(line.strip().split()[2])
-        elif line.strip().split()[1] == 'cd':
-          ascend()
-        elif line.strip().split()[1] == 'ls':
-          read_until_next_command()
-        else:
-          print(f"error, unknown command: {line.strip()}")
+    return list(map(str.strip, list(file)))
 
-  return data
+print(f"{readfile()[0]}")
+#>>> a = {}
+#>>> a['/'] = {}
+#>>> a
+#{'/': {}}
+#>>> a['/']['bqpslnv'] = 113975
+#>>> a['/']['..'] = a['/']
+#>>> a
+#{'/': {'bqpslnv': 113975, '..': {...}}}
