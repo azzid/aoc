305aab8a686eb7bbb72e184caeacb109f1613713
Mattias Fransson <mattias@webben.nu>
2023-12-11 22:22:28 +0100

    That's the right answer! You are one gold star closer to restoring snow operations.

--- 2023/aoc01.py
+++ 2023/aoc01.py
@@ -6,7 +6,12 @@ def datafilename():
   return txtpath
 
 def readfile():
+  data = []
   datafile = datafilename()
   with open(datafile, 'r') as file:
     for line in file:
+      nums = [ c for c in line if c.strip().isdigit() ]
+      data.append(int(nums[0] + nums[-1]))
   return data
+
+print(sum(readfile()))
