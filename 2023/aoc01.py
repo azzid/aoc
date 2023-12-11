7e6be6be491156b7049fa62f34def18a605bb151
Mattias Fransson <mattias@webben.nu>
2023-12-11 21:59:59 +0100

    2023 is on!

---
+++ 2023/aoc01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from pathlib import Path
+def datafilename():
+  mypath = Path(__file__)
+  txtpath = mypath.with_suffix('.txt')
+  return txtpath
+
+def readfile():
+  datafile = datafilename()
+  with open(datafile, 'r') as file:
+    for line in file:
+  return data
