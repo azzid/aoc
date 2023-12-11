219e9bda9c428bc6cac758ee08356db0227c2d61
Mattias Fransson <mattias@webben.nu>
2023-12-11 22:56:12 +0100

    Answer: 52606
    That's not the right answer; your answer is too low. If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit. Please wait one minute before trying again.

--- 2023/aoc01.py
+++ 2023/aoc01.py
@@ -10,7 +10,8 @@ def readfile():
   datafile = datafilename()
   with open(datafile, 'r') as file:
     for line in file:
-      nums = [ c for c in line if c.strip().isdigit() ]
+      digitedline = line.replace('one', '1').replace('two', '2').replace('three','3').replace('four','4').replace('five','5').replace('six','6').replace('seven','7').replace('eight','8').replace('nine','9')
+      nums = [ c for c in digitedline if c.isdigit() ]
       data.append(int(nums[0] + nums[-1]))
   return data
 
