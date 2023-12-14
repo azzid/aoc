597e63653bc83954e0622fc5a7e5eb33a1fe8a4d
Mattias Fransson <mattias@webben.nu>
2023-12-14 17:56:37 +0100

    input data #3

---
+++ 2023/aoc03.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from pathlib import Path
+def datafilename():
+  mypath = Path(__file__)
+  txtpath = mypath.with_suffix('.txt')
+  return txtpath
+
+def readfile():
+  data = []
+  datafile = datafilename()
+  with open(datafile, 'r') as file:
+    for line in file:
+
+  return data
+
+print(readfile())
