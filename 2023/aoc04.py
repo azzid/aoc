b7d379b330229837e526e9db96c76590ca78ccd6
Mattias Fransson <mattias@webben.nu>
2023-12-18 19:36:31 +0100

    Answer: 23235
    That's the right answer!

--- 2023/aoc04.py
+++ 2023/aoc04.py
@@ -10,5 +10,17 @@ def readfile():
   datafile = datafilename()
   with open(datafile, 'r') as file:
     for line in file:
-      
+      draw = {}
+      winner, ticket = line.split(':')[1].split('|')
+      draw['winner'] = winner.split()
+      draw['ticket'] = ticket.split()
+      data.append(draw)
   return data
+
+points = 0
+for draw in readfile():
+  numwinnums = len(set(draw['winner']).intersection(set(draw['ticket'])))
+  #print(numwinnums)
+  if numwinnums > 0:
+    points = points + 2**(numwinnums-1)
+print(points)
