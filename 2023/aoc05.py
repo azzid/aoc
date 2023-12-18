3607caf24c600398bae59e94b446dfe4cf7f73ea
Mattias Fransson <mattias@webben.nu>
2023-12-18 22:42:47 +0100

    Will take forever to finish
    enumerating all the maps will probably blow all available memory
    as well - need to re-implement with only max/min values for the ranges.

--- 2023/aoc05.py
+++ 2023/aoc05.py
@@ -8,7 +8,31 @@ def datafilename():
 def readfile():
   data = []
   datafile = datafilename()
+  source, destination = 'unset', 'unset'
   with open(datafile, 'r') as file:
     for line in file:
+      if 'seeds:' in line:
+        print(f"seeds: {line.split(':')[1].split()}")
+      elif 'map:' in line:
+        print(f"{line.split()[0].split('-to-')}")
+        source, destination = line.split()[0].split('-to-')
+      elif '\n' == line:
+        if  not (source, destination) == ('unset', 'unset'):
+          a['source'] = source
+          a['destination'] = destionation
+          data.append(a)
+        a = {}
+        #pass
+        #print(f"empty line, source is {source} - dest is {destination}")
+      else: # numbers...
+        dststart, srcstart, length = list(map(int, line.split()))
+        srclist = list(range(srcstart, srcstart+length))
+        dstlist = list(range(dststart, dststart+length))
+        for i, srcid in enumerate(srclist):
+          a[srcid] = dstlist[i]
+        print(dststart, srcstart, length)
+        #pass
 
   return data
+
+print(readfile())
